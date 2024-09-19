191f7ae32e9b264c4b0658429cea5e36e7083862
Pierre Lebedel <PierreLebedel@users.noreply.github.com>
2024-09-19 17:12:30 +0200

    Delete Envoy.blade.php
    Not in prod

--- Envoy.blade.php
+++
@@ -1,8 +0,0 @@
-@servers(['web' => ['pleb@51.210.9.18 -p 2223']])
-
-@task('deploy', ['on' => 'web'])
-    cd /var/www/tallcomponents;
-    git pull origin main;
-    php artisan migrate --force;
-    php artisan optimize:clear;
-@endtask
